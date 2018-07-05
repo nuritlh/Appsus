6315fcaf4146c78c008d1f4949982a699d41d2c1
omers <omer3026@gmail.com>
2018-07-05 16:01:54 +0300

    margin

--- js/pages/home-page-cmps.js
+++ js/pages/home-page-cmps.js
@@ -5,7 +5,7 @@ export default {
 
     template:`
     <section class="home-page">
-        <h1>welcome to apsus</h1>
+      
         <div class="flex align-center space-around">
             <div  class="route-box email-route flex align-center">
                 <router-link exact to="/emailApp">email</router-link>
