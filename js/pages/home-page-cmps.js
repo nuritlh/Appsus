ede5b64cec216c9354f8ebff5463c901107736eb
omers <omer3026@gmail.com>
2018-07-05 15:41:18 +0300

    basic style

--- js/pages/home-page-cmps.js
+++ js/pages/home-page-cmps.js
@@ -6,11 +6,11 @@ export default {
     template:`
     <section class="home-page">
         <h1>welcome to apsus</h1>
-        <div class="email-route">
-            <div>
+        <div class="flex align-center space-around">
+            <div  class="route-box email-route flex align-center">
                 <router-link exact to="/emailApp">email</router-link>
             </div>
-            <div class="kepper-route">
+            <div class="route-box kepper-route flex align-center">
                 <router-link exact to="/kepperApp">kepper</router-link>
             </div>
         </div>
